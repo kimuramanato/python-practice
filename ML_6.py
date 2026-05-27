83f0747d232a5f5901ea60238898650fc5012d7c
kimuramanato <shuukatu.kimu12@gmail.com>
2026-05-27 22:18:42 +0900

    修正した

--- ML_6.py
+++ ML_6.py
@@ -1,8 +1,9 @@
 from sklearn.utils import resample
+import numpy as np
 data=np.array([1,2,3,4,5])
 n_bootstrap_samples=3
 x=[resample(data,replace=True, n_samples=len(data),random_state=i) for i in range(n_bootstrap_samples)]
 
 sorted_bootstrap_samples = [np.sort(sample) for sample in x]
 
-sorted_bootstrap_samples
+print(sorted_bootstrap_samples)
